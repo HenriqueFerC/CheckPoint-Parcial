deeeffd6835393c872fd035975d7b08c37cec758
HenriqueFerC <rm552325@fiap.com.br>
2023-09-06 17:59:12 -0300

    Last commit

--- exercise list/src/main.jsx
+++ exercise list/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import EditarProdutos from './routes/EditarProdutos/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
@@ -25,6 +25,10 @@ const router = createBrowserRouter([
       {
         path: "/produtos/editar/:id",
         element: <EditarProdutos/>
+      },
+      {
+        path: "/antiga",
+        element: <Navigate to="/"/>
       }
     ]
   }
